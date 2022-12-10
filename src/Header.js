fd9facb52d1051a6cb9c3b467d3dd6112286d4b1
Sebastian Wharton <sebastian.wharton1@gmail.com>
2022-12-10 03:25:27 -0600

    Tailwind React Installation

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <>
+      <header className="flex align-middle justify-center">
+        <h1 className="bg-slate-600">Plant Shop</h1>
+        
+      </header>
+    </>
+  );
+};
+
+export default Header;
